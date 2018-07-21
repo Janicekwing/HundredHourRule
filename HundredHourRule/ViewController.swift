c13e83a8de608652d2566fb2edee76a5aa45d574
Janice He <janicekwhe@gmail.com>
2018-07-21 15:57:27 -0400

    Second View Controller is created

--- HundredHourRule/ViewController.swift
+++ HundredHourRule/ViewController.swift
@@ -21,7 +21,9 @@ class ViewController: UIViewController {
     }
 
     @IBAction func startButton(_ sender: Any) {
+        performSegue(withIdentifier: "Segue", sender: self)
     }
+    
     
     
 }
