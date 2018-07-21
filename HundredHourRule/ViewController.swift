0ab2fd66dd5b2aa6dc531c1da979f1da05fc62f5
Janice He <janicekwhe@gmail.com>
2018-07-21 10:42:20 -0400

    startButton linked with IBAction function

--- HundredHourRule/ViewController.swift
+++ HundredHourRule/ViewController.swift
@@ -20,7 +20,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBOutlet weak var startButton: UILabel!
+    @IBAction func startButton(_ sender: Any) {
+    }
+    
     
 }
 
